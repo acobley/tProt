a640b6cc56cffb89215e814c15b611264b0992f2
AndyCobley <acobley@computing.dundee.ac.uk>
2012-03-16 15:54:16 +0000

    added convertors util

---
+++ src/uk/ac/dundee/computing/aec/util/Convertors.java
@@ -0,0 +1,43 @@
+package uk.ac.dundee.computing.aec.util;
+
+import java.net.URLDecoder;
+import java.util.StringTokenizer;
+
+import javax.servlet.http.HttpServletRequest;
+
+public final class Convertors {
+	public static String[] SplitRequestPath(HttpServletRequest request){
+		String args[] = null;
+		 
+			
+		StringTokenizer st = SplitString(request.getRequestURI());
+		args = new String[st.countTokens()];
+		//Lets assume the number is the last argument
+		
+		int argv=0;
+		while (st.hasMoreTokens ()) {;
+			args[argv]=new String();
+						
+			args[argv]=st.nextToken();
+			try{
+				//System.out.println("String was "+URLDecoder.decode(args[argv],"UTF-8"));
+				args[argv]=URLDecoder.decode(args[argv],"UTF-8");
+				
+			}catch(Exception et){
+				System.out.println("Bad URL Encoding"+args[argv]);
+			}
+			argv++;
+			} 
+
+	//so now they'll be in the args array.  
+	// argv[0] should be the user directory
+	
+		return args;
+		}
+		
+	  private static StringTokenizer SplitString(String str){
+	  		return new StringTokenizer (str,"/");
+
+	  }
+
+}
