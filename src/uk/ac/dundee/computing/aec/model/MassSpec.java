569ecd692def40183ebce2584b64def4e4a49f9b
AndyCobley <acobley@computing.dundee.ac.uk>
2012-03-18 14:08:23 +0000

    Points in MAssSpec

--- src/uk/ac/dundee/computing/aec/model/MassSpec.java
+++ src/uk/ac/dundee/computing/aec/model/MassSpec.java
@@ -1,8 +1,17 @@
 package uk.ac.dundee.computing.aec.model;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.awt.Point;
+
 public class MassSpec {
 	public MassSpec(){
 		
 	}
 
+	
+	List <Point> getMassSpec(){
+		//This is a dummy class returning a Pseudo MAssSpec
+		List <Point> Points= new LinkedList<Point>();
+	}
 }
