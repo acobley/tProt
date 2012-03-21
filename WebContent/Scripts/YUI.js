259e959ebf66f42d8049219ef55def3a36632922
AndyCobley <acobley@computing.dundee.ac.uk>
2012-03-21 12:24:33 +0000

    fixing tab display

--- WebContent/Scripts/YUI.js
+++ WebContent/Scripts/YUI.js
@@ -9,26 +9,26 @@ YUI().use('tabview', function (Y) {
 	var tabview = new Y.TabView({
         srcNode: '#demo'
     });
-
+	tabview.after("selectionChange", function(event) {
+	    // Do something just before "enabled" is about to be set
+		 var prev =event.prevVal;
+         var newv =event.newVal;
+		YUI().use('charts', function (Y) {
+		    // Charts is available and ready for use. Add implementation
+		    // code here.
+			// Instantiate and render the chart
+           
+		    loadValues(Y);
+		    loadIds(Y);
+		    loadCentroids(Y);
+			
+		});
+	});
     tabview.render();
 });
 
 
 
-
-
-
-//Create a new YUI instance and populate it with the required modules.
-YUI().use('charts', function (Y) {
-    // Charts is available and ready for use. Add implementation
-    // code here.
-	// Instantiate and render the chart
-
-    loadValues(Y);
-    loadIds(Y);
-    loadCentroids(Y);
-	
-});
 
 
                 
